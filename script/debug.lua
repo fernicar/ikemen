a5018de71c59b835fbfa6e600502bd70ff73f6c8
SUEHIRO <supersuehiro@users.sourceforge.jp>
2012-04-24 14:25:57 +0000

    bindの処理を修正。それとデバッグ用のスクリプトに関する変更
    git-svn-id: https://ikemen.googlecode.com/svn/trunk@787 34fca844-09ac-11df-9859-c12d74d802c5

--- script/debug.lua
+++ script/debug.lua
@@ -1,4 +1,76 @@
 ﻿
+addHotkey('c', true, false, false, 'toggleClsnDraw()')
+addHotkey('d', true, false, false, 'toggleDebugDraw()')
+addHotkey('s', true, false, false, 'changeSpeed()')
+addHotkey('l', true, false, false, 'toggleStatusDraw()')
+addHotkey('1', true, false, false, 'toggleAI(1)')
+addHotkey('2', true, false, false, 'toggleAI(2)')
+addHotkey('3', true, false, false, 'toggleAI(3)')
+addHotkey('4', true, false, false, 'toggleAI(4)')
+addHotkey('F1', false, false, false, 'kill(2);kill(4)')
+addHotkey('F1', true, false, false, 'kill(1);kill(3)')
+addHotkey('F2', false, false, false, 'kill(1,1);kill(2,1);kill(3,1);kill(4,1)')
+addHotkey('F2', true, false, false, 'kill(1,1);kill(3,1)')
+addHotkey('F2', false, false, true, 'kill(2,1);kill(4,1)')
+addHotkey('F3', false, false, false, 'powMax(1);powMax(2)')
+addHotkey('F4', false, false, false, 'roundReset()')
+addHotkey('F4', false, false, true, 'reload()')
+addHotkey('F5', false, false, false, 'setTime(0)')
+addHotkey('SPACE', false, false, false, 'full(1);full(2);full(3);full(4)')
+addHotkey('PAUSE', false, false, false, 'togglePause()')
+addHotkey('SCROLLOCK', false, false, false, 'step()')
+
+
+speed = 1.0
+
+function changeSpeed()
+  if speed >= 4 then
+    speed = 0.25
+  else
+    speed = speed*2.0
+  end
+  setAccel(speed)
+end
+
+function toggleAI(p)
+  local oldid = id()
+  if player(p) then
+    if ailevel() > 0 then
+      setAILevel(0)
+    else
+      setAILevel(8)
+    end
+    playerid(oldid)
+  end
+end
+
+function kill(p, ...)
+  local oldid = id()
+  if player(p) then
+    local n = ...;
+    if not n then n = 0 end
+    setLife(n)
+    playerid(oldid)
+  end
+end
+
+function powMax(p)
+  local oldid = id()
+  if player(p) then
+    setPower(powermax())
+    playerid(oldid)
+  end
+end
+
+function full(p)
+  local oldid = id()
+  if player(p) then
+    setLife(lifemax())
+    setPower(powermax())
+    playerid(oldid)
+  end
+end
+
 function info()
   puts(
     string.format(
